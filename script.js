834fafcd47ff59eaa9a3937752b358276a2eaf76
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-29 12:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  getArea() {
+    console.log(this.width * this.height);
+  }
+  getWidth() {
+    console.log(this.width);
+  }
+  getheigth() {
+    console.log(this.height);
+  }
+}
+class Square extends Rectangle {
+  constructor(side) {
+   super(side,side)
+  }
+  perimeter() {
+    console.log(4 * this.width);
+  }
+}
 
-class Square extends Animal {}
+let sq = new Square(2);
+sq.getArea();
+
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
